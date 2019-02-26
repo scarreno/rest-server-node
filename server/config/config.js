0e5d08048c6ab1c0622c08cdae95b48ff5fda11c
sergioc <sergioc@lagash.com>
2019-02-26 16:16:38 -0300

    deploy

--- server/config/config.js
+++ server/config/config.js
@@ -22,4 +22,6 @@ if (process.env.NODE_ENV === 'dev') {
     urlDb = 'mongodb://dbuser:admin1234@udemy-cafe-shard-00-00-hkcs2.mongodb.net:27017,udemy-cafe-shard-00-01-hkcs2.mongodb.net:27017,udemy-cafe-shard-00-02-hkcs2.mongodb.net:27017/cafe?ssl=true&replicaSet=udemy-cafe-shard-0&authSource=admin&retryWrites=true';
 }
 
+
+
 process.env.URL_DB = urlDb;
