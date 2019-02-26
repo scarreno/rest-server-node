7fb9b596c61c78595721769b7ca033ee0ff8c155
sergioc <sergioc@lagash.com>
2019-02-26 16:57:13 -0300

    delete prod con string

--- server/config/config.js
+++ server/config/config.js
@@ -19,9 +19,7 @@ let urlDb;
 if (process.env.NODE_ENV === 'dev') {
     urlDb = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDb = 'mongodb://dbuser:admin1234@udemy-cafe-shard-00-00-hkcs2.mongodb.net:27017,udemy-cafe-shard-00-01-hkcs2.mongodb.net:27017,udemy-cafe-shard-00-02-hkcs2.mongodb.net:27017/cafe?ssl=true&replicaSet=udemy-cafe-shard-0&authSource=admin&retryWrites=true';
+    urlDb = process.env.MONGO_URI;
 }
-
-
 
 process.env.URL_DB = urlDb;
